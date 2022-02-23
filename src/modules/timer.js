b3c01c3a759e84df0b852ec50bea02e4483ec1e1
Begal Wadim <begal-vadim98@yandex.ru>
2022-02-23 13:14:07 +0300

    Правки по дз

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -5,6 +5,14 @@ const timer = (deadline) => {
     timerMinutesours = document.getElementById('timer-minutes'),
     timerSeconds = document.getElementById('timer-seconds');
 
+  const addZero = (num) => {
+      if (num <= 9) {
+          return '0' + num;
+      } else {
+          return num;
+      }
+  };
+
   const getTimerRemaining = () => {
     let dateStop = new Date(deadline).getTime();
     let dateNow = new Date().getTime();
@@ -27,18 +35,26 @@ const timer = (deadline) => {
 
   const updateClock = () => {
 
+    const timerAction = document.querySelector('.timer-action');
+
     let getTime = getTimerRemaining();
 
-    for (let key in getTime) {
-      if (getTime[key] < 10) getTime[key] = "0" + getTime[key];
-    }
 
-    if (getTime.timerRemaining > 0) {
-      timerDays.textContent = getTime.days;
-      timerHours.textContent = getTime.hours;
-      timerMinutesours.textContent = getTime.minutes;
-      timerSeconds.textContent = getTime.seconds;
-    } else clearInterval(idIntervalTimer);
+      timerDays.textContent = addZero(getTime.days);
+      timerHours.textContent = addZero(getTime.hours);
+      timerMinutesours.textContent = addZero(getTime.minutes);
+      timerSeconds.textContent = addZero(getTime.seconds);
+    
+      if (getTime.timerRemaining <= 0) {
+        timerDays.textContent = "00";
+        timerHours.textContent = "00";
+        timerMinutesours.textContent = "00";
+        timerSeconds.textContent = "00";
+        timerAction.textContent = "Акция закончилась";
+
+        clearInterval(idIntervalTimer);
+    }
+     
   }
 
   let idIntervalTimer = setInterval(updateClock, 1000)
